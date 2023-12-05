1d5721f59ccb1a897fbf4b5d011eca7dc38d80cf
Cel Pynenborg <cel.pynenborg@telenetgroup.be>
2023-12-05 10:06:25 +0100

    Day 4 Part 2

--- day-4/puzzle_two.py
+++ day-4/puzzle_two.py
@@ -1,25 +1,19 @@
-import math
 import re
 
 NUM_PATTERN = re.compile(r'\d+')
 
 
 def solve(lines: list[str]) -> int:
-    points = []
-    for line in lines:
+    cards = [0] * len(lines)
+    for index, line in enumerate(lines):
+        cards[index] += 1
         split = line.split('|')
         winning = split[0].split(':')[1]
         winning = set([winning[match.start():match.end()] for match in NUM_PATTERN.finditer(winning)])
         actual = [split[1][match.start():match.end()] for match in NUM_PATTERN.finditer(split[1])]
 
-        winning_count = 0
-        for n in actual:
-            if n in winning:
-                winning_count += 1
+        win_count = sum([1 for n in actual if n in winning])
+        for i in range(1, win_count + 1):
+            cards[index + i] += (1 * cards[index])
 
-        if winning_count == 1:
-            points.append(1)
-        elif winning_count > 0:
-            points.append(int(math.pow(2, winning_count - 1)))
-
-    return sum(points)
+    return sum(cards)
